e1ce49e828c266c8a6bc1a45419bdc878303c7e1
Mikhail Beloborodov <rkruzo19@gmail.com>
2024-04-14 10:32:18 +0300

    Input + Output

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,73 @@
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("input.md");
+        FileWriter fw = new FileWriter("output.md");
         Scanner sc = new Scanner(fr);
-        String s = sc.nextLine();
-        sc.close();
-        System.out.println(s);
-        fr.close();
-        double a = 0.0;
-        double b = 0.0;
-        char sym = ' ';
-        int m = 0;
-        boolean t = true;
-
-        for (String s2 : s.split(" ")) {
-            m++;
-            if (m == 1) {
-                try {
-                    a = Double.valueOf(s2);
-                } catch (Exception e) {
-                    System.out.println("Error! Not number");
-                    t = false;
-                    break;
-                }
-            }
-            if (m == 3) {
-                try {
-                    b = Double.valueOf(s2);
-                } catch (Exception e2) {
-                    System.out.println("Error! Not number");
-                    t = false;
-                    break;
-                }
-            }
-        }
-        if (t) {
-            m = 0;
-            for (String s3 : s.split(" ")) {
+        String s;
+        while (sc.hasNextLine()) {
+            s = sc.nextLine();
+            double a = 0.0;
+            double b = 0.0;
+            char sym = ' ';
+            int m = 0;
+            boolean t = true;
+            for (String s2 : s.split(" ")) {
                 m++;
-                if (m == 2) {
+                if (m == 1) {
                     try {
-                        sym = result(s3, sym);
-                    } catch (Exception e3) {
-                        System.out.println("Operation Error!");
+                        a = Double.valueOf(s2);
+                    } catch (Exception e) {
+                        System.out.println(s + " - Error! Not number");
+                        t = false;
+                        break;
+                    }
+                }
+                if (m == 3) {
+                    try {
+                        b = Double.valueOf(s2);
+                    } catch (Exception e2) {
+                        System.out.println(s + " - Error! Not number");
+                        t = false;
+                        break;
+                    }
+                }
+            }
+            if (t) {
+                m = 0;
+                for (String s3 : s.split(" ")) {
+                    m++;
+                    if (m == 2) {
+                        try {
+                            sym = result(s3, sym);
+                        } catch (Exception e3) {
+                            System.out.println(s + " - Operation Error!");
+                        }
+                    }
+                }
+            }
+            if (t) {
+                if (sym == '+') fw.write(String.format("%s + %s = %s\n", a, b, (a + b)));
+                if (sym == '-') fw.write(String.format("%s - %s = %s\n", a, b, (a - b)));
+                if (sym == '*') fw.write(String.format("%s * %s = %s\n", a, b, (a * b)));
+                if (sym == '/') {
+                    if (b == 0) {
+                        System.out.println(s + " - Error! Division by zero");
+                    } else {
+                        fw.write(String.format("%s / %s = %s\n", a, b, (a / b)));
                     }
                 }
             }
         }
-        if (t) {
-            if (sym == '+') System.out.println(a + b);
-            if (sym == '-') System.out.println(a - b);
-            if (sym == '*') System.out.println(a * b);
-            if (sym == '/') System.out.println(b == 0 ? "Error! Division by zero" : a / b);
-        }
+        sc.close();
+        fr.close();
+        fw.close();
+
 
     }
 
